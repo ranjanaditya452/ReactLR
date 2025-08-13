2eb0ff77e20fa729138fa5b1eb98d8b4796a8ef0
Aditya <ranjanaditya452@gmail.com>
2025-08-13 19:58:17 +0530

    js func 1

--- JSbasic1st/js1.js
+++ JSbasic1st/js1.js
@@ -1,5 +1,4 @@
-Arr = ["Apple","Beer","Cold",["Swamp",["Swamp","Tranquil"],"Tundra"],"Fire",["Death"]]
-Arr2 = ["Oats","Swamp","Tundra"]
-Arr.push(Arr2)
-console.log(Arr);
-console.log(Arr.flat(2) )
+let dsd = () =>{
+    alert("hi");
+}
+dsd();
