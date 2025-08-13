fc7b63a57745b397e9cb430d05b5db7d06e72f55
Aditya <ranjanaditya452@gmail.com>
2025-08-13 23:16:58 +0530

    Callback func in js

--- JSbasic1st/js1.js
+++ JSbasic1st/js1.js
@@ -1,4 +1,36 @@
-let dsd = () =>{
-    alert("hi");
+
+// alert(asp);
+// let greeter = (name,def)=>{
+//     alert( `${name} is a ${def}`);
+// }
+// greeter(2,"asperg");
+
+// var asp="d";
+// alert(asp);
+
+function test(value)
+{
+console.log(typeof value)
 }
-dsd();
+
+
+function greet(name)
+{
+console.log(`Welcome ${name}`)
+}
+
+test(greet);
+
+function lunch(starter,maincourse,dessert)
+{
+    console.log(`I had ${starter} as a starter for lunch`)
+    console.log(`I had ${maincourse} as a maincourse for lunch`)
+    dessert();
+}
+
+function starbucks()
+{
+    console.log('hi')
+}
+
+lunch('veg crispy','Mutter Paneer', starbucks)
