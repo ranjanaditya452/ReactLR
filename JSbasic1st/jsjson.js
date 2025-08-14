037e9ea036f23a7366d3b9581f8e3ffcca2b2510
Aditya <ranjanaditya452@gmail.com>
2025-08-15 00:24:43 +0530

    Obj basics and adv array mthods

--- JSbasic1st/jsjson.js
+++ JSbasic1st/jsjson.js
@@ -250,8 +250,28 @@ const prod= products.filter(product=>
 const sum = products.reduce((acc,prod)=>
     acc+=prod.price,0)
 
-//display title of all mens clothing
+//display total cost of all mens clothing
 const prod1 = products.filter(product=>product.category ==="men's clothing")
-.map(product=>product.title);
+.map(product=>product.price).reduce((accum,prod)=>accum+=prod,0);
 
-console.log(prod1)
+//display all shirt products
+
+const shirtproducts = products.filter(product => product.title.toLowerCase().includes("jacket"));
+ 
+//fetch all categories
+const allcategories = products.map(product=>product.category)
+let set = new Set(allcategories);
+let arrcat=Array.from(set)
+let obj={};
+for(let i=0;i<arrcat.length;i++)
+{
+  let ab= products.filter(prod=>prod.category===arrcat[i])
+  obj[arrcat[i]]=ab;
+}
+// all electronic ratings
+let count=0;
+const elecratings = products.filter(prod => prod.category==='electronics').map(elecs=>{
+  count++;
+  return elecs.rating.rate;
+}).reduce((acc,rats)=>acc+=rats,0)
+console.log(elecratings/count)
